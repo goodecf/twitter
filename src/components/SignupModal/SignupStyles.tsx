d8755cba2a01c977f93e689d36600503117e923c
Cameron Goode <cgoode101@gmail.com>
2020-12-23 11:40:58 -0500

    update signup modal styles

--- src/components/SignupModal/SignupStyles.tsx
+++ src/components/SignupModal/SignupStyles.tsx
@@ -1,5 +1,5 @@
 import styled from 'styled-components';
-import { Modal, Button, Image, Form } from 'semantic-ui-react'
+import { Modal, Button, Image, Form, Input, Icon } from 'semantic-ui-react'
 
 export const SignupModal = styled(Modal)`
   && {
@@ -19,17 +19,35 @@ export const ModalHeader = styled(Modal.Header)`
   }
 `;
 
-export const Icon = styled(Image)`
+export const TwitIcon = styled(Image)`
   &&& {
-    margin: 0px auto 0px 50%;
     width: 25px;
     height: 25px;
+    margin-left: auto;
+  }
+`;
+
+export const TwitIconCenter = styled(TwitIcon)`
+  &&& {
+    margin-right: auto;
+    margin-left: 0px;
+  }
+`;
+
+export const IconPageTwo = styled(Icon)`
+  && {
+    align-self: flex-start;
+    margin: 0px auto 0px 0px;
+    padding: 0px;
+    width: 25px;
+    height: 25px;
+    cursor: pointer;
   }
 `;
 
 export const NextBtn = styled(Button)`
   && {
-    margin: 0px;
+    margin: 0px 0px 0px auto;
     width: 75px;
     max-width: 75px;
     border-radius: 15%;
@@ -38,17 +56,21 @@ export const NextBtn = styled(Button)`
 `;
 
 export const SignupForm = styled(Form)`
-  && {
-    width: 50%;
+  &&&&&& {
+    display: flex;
+    flex-direction: column;
   }
 `;
 
 export const Text = styled.p`
   display: block;
-  margin-bottom: 0px;
   font-size: 16px;
   color: gray;
   margin-bottom: 15px;
+`;
+
+export const TextSecondPage = styled(Text)`
+  margin: 20px 0px 30px 0px;
 `;
 
 export const TextBold = styled(Text)`
@@ -62,4 +84,23 @@ export const Label = styled.label`
   font-weight: bold;
   display: block;
   margin-bottom: 5px;
+`;
+
+export const SignupButton = styled(Button)`
+  &&& {
+    width: 100%
+  }
+`;
+
+export const Container = styled(Modal.Content)`
+  &&&& {
+    display: flex
+    flex-direction: column;
+  }
+`;
+
+export const LabelInput = styled(Input)`
+  && {
+    margin-bottom: 10px;
+  }
 `;
